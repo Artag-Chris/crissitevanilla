c42601e591fe2e29690c691c5f59b5bf30bf81b7
Artag-Chris <74101396+Artag-Chris@users.noreply.github.com>
2021-05-22 16:59:51 -0500

    adding

---
+++ sitioweb/script.js
@@ -0,0 +1,17 @@
+
+/*
+let options = [
+    "Node.js es un entorno de tiempo de ejecución de JavaScript (de ahí su terminación en .js haciendo alusión al lenguaje JavaScript). Este entorno de tiempo de ejecución en tiempo real incluye todo lo que se necesita para ejecutar un programa escrito en JavaScript. También aporta muchos beneficios y soluciona muchísimos problemas, por lo que sería más que interesante realizar nuestro curso de Node.js para obtener las bases, conceptos y habilidades necesarias que nos motiven a profundizar en sus opciones e iniciar la programación. ", "aqui hay mas text"
+]
+
+
+function prueba(){
+   let random = Math.floor(Math.random() * options.length);
+    document.getElementById("doThis").innerHTML = options[random];
+   
+   
+   
+}
+function cerrar(){
+    document.getElementById("doThis").display ="none"
+}*/
---
+++ sitioweb/script2.js
@@ -0,0 +1,12 @@
+
+/*const doThis = document.getElementById("doThis");
+let newThing = document.createElement("p");
+newThing.createTextNode = "hola hermosino";
+
+
+doThis.appendChild(newThing);*/
+
+const body = document.body
+const div = document.createElement("div");
+div.innerText=" hola hermoso"
+body.append(div)
